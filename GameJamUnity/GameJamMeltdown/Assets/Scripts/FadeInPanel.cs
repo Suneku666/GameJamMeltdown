8753313b26f8db4a7f00df8275ceb8a2b7be7f43
Suneku666 <magiera43@gmail.com>
2017-11-05 11:15:37 +0000

    setup some basic menu and scene setup

---
+++ GameJamUnity/GameJamMeltdown/Assets/Scripts/FadeInPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInPanel : MonoBehaviour {
+
+    public float fadeInTime = 1.5f;
+
+    private Image fadePanel;
+    private Color CurrentColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+        fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad < fadeInTime)
+        {
+            //Fade in
+            float alphaChange = Time.deltaTime / fadeInTime;
+            CurrentColor.a -= alphaChange;
+            fadePanel.color = CurrentColor;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
---
+++ GameJamUnity/GameJamMeltdown/Assets/Scripts/PlayerPrefManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefManager : MonoBehaviour {
+
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+    
+    //level_unlocked_ + levelNumber
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume > 0f && volume < 1f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Master volume out of range");
+        }
+    }
+
+    public static float GetMastervolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    public static void UnlockLevel(int level)
+    {
+        if(level <= Application.levelCount - 1)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), level);
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in the build order");
+        }
+    }
+
+    public static bool IsLevelUnlocked(int level)
+    {
+        if (level <= Application.levelCount - 1)
+        {
+            if (PlayerPrefs.GetInt(LEVEL_KEY + level.ToString()) == level)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            Debug.LogError("no such level exists in build order : " + level);
+        }
+
+        return false;
+    }
+
+    public static void SetDifficulty(float difficultyLevel)
+    {
+        if (0 <= difficultyLevel && difficultyLevel <= 1)
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficultyLevel);
+        }
+        else
+        {
+            Debug.LogError("Saving Difficulty preference unsuccessfull with value : " + difficultyLevel);
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+    }
+}
